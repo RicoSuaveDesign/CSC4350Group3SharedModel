ee2d110c4400efdf529923281cf2210fbbda20c9
James Ratini <jratini@carthage.edu>
2017-10-11 12:18:57 -0500

    Fixed characteristic
    Logic behind identifying when a string is invalid was incorrect. Was evaluating success after first characters when numerous leading spaces could have occured, which is valid.

--- James-Characteristic.cpp
+++ James-Characteristic.cpp
@@ -8,7 +8,7 @@ bool characteristic(char number[], int& c);
 bool characteristic(char numString[], int& c)
 {
 	char *ptr;
-	bool firstChar = true;
+	bool seenNumber = false;
 	ptr = numString;
 	// Iterate through number looking for '.' or '\0'. If '.', then preceding characters are characteristic
 	// If '\n', then whole number must be characteristic
@@ -16,15 +16,19 @@ bool characteristic(char numString[], int& c)
 	while(*ptr != '.' && *ptr != '\0')
 	{
 		// If not the first char and char is '-', '+', or ' ', then invalid
-		if(firstChar == false && (*ptr == '-' || *ptr == '+' || *ptr == ' '))
+		if( seenNumber == true && (*ptr == '-' || *ptr == '+'))
 		{
 			//Invalid string
 			c = -1;
 			return false;
 		}
 
+		if( seenNumber == false && (*ptr != ' ' && *ptr != '+' && *ptr != '-'))
+		{
+			seenNumber = true;
+		}
 		ptr++;
-		firstChar = false;
+		
 	}
 
 	// atoi seems like cheating
