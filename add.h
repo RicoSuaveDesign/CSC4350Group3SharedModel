e967280889a61096d98875129df155f23130017a
Daniel Schechtman <dschechtman@carthage.edu>
2017-10-13 23:57:42 -0500

    Add files via upload

--- add.h
+++ add.h
@@ -44,40 +44,6 @@ void howManyTimeDenominatorGoesIntoNumerator(int &multiply, int &denominator, in
 
 char *createStringRepOfFloat(char *characteristic, char *mantissa);
 
-
-bool characteristic(char number[], int& c);
-
-bool characteristic(char numString[], int& c)
-{
-    char *ptr;
-    bool seenNumber = false;
-    ptr = numString;
-    // Iterate through number looking for '.' or '\0'. If '.', then preceding characters are characteristic
-    // If '\n', then whole number must be characteristic
-
-    while(*ptr != '.' && *ptr != '\0')
-    {
-        // If not the first char and char is '-', '+', or ' ', then invalid
-        if( seenNumber == true && (*ptr == '-' || *ptr == '+'))
-        {
-            //Invalid string
-            c = -1;
-            return false;
-        }
-
-        if( seenNumber == false && (*ptr != ' ' && *ptr != '+' && *ptr != '-'))
-        {
-            seenNumber = true;
-        }
-        ptr++;
-
-    }
-
-    // atoi seems like cheating
-    c = atoi(numString);
-    return true;
-}
-
 // HELPER FUNCTIONS FOR add
 char* makeString(int len){
     // I add 2 to len, so the c_string will have room for a unary sign ('+' or '-')
